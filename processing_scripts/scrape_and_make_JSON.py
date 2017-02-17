67e23164dcb11d57d53ac4250e27cb74c13db0f1
ChemiKyle <Kyle.2493@gmail.com>
2017-02-16 20:17:52 -0500

    Made web ready, pretty formatting for drop down lists.

--- processing_scripts/scrape_and_make_JSON.py
+++ processing_scripts/scrape_and_make_JSON.py
@@ -12,8 +12,7 @@ author_regex = re.compile(r'''(
 	\)					# Close paren
 	)''', re.VERBOSE)
 
-notes_filename = 'My Clippings.txt'
-
+notes_filename = 'My_Clippings.txt'
 list_of_authors = []
 
 with open(notes_filename, 'r') as f:
@@ -28,26 +27,26 @@ with open(notes_filename, 'r') as f:
 big_dict = {}
 auth_and_book_list = []
 
-auth_num = 0
-quote_num = 0
-
 for author in list_of_authors:
-	big_dict['little_dict' + str(auth_num)] = {}
+	quote_num = 0
+	big_dict[str(author)] = {}
 	with open(notes_filename, 'r+') as f:
 		for line in f:
 			if str(author) in line:
-				big_dict['little_dict' + str(auth_num)]['quote_num' + str(quote_num)] = {}
 				book_name = line.split('(')[0].strip()
-				if (author, book_name) not in auth_and_book_list: #Store author and book name as a tuple, in case I need this for the dict
-					auth_and_book_list.append((author, book_name))
-				big_dict['little_dict' + str(auth_num)]['quote_num' + str(quote_num)].update({
-				'author_name': author,
-				'book_name': book_name,
-				'quote': 'coming soon'
+				try:
+					print(big_dict[str(author)][book_name])
+				except:
+					big_dict[str(author)][book_name] = {}
+				big_dict[str(author)][book_name].update({
+				'quote' + str(quote_num): 'coming soon'
 				})
 				quote_num += 1
-	auth_num +=1
 
 # print(auth_and_book_list[1:])
 jayson = json.dumps(big_dict)
 print(jayson)
+print(big_dict[str(author)][book_name])
+with open('JSON_DATA.json', 'w') as f:
+	for i in jayson:
+		f.write(i)
