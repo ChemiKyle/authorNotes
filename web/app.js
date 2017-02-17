5d2bea77d413bbc73e8396df3f7bf8e45b82a488
ChemiKyle <Kyle.2493@gmail.com>
2017-02-17 18:08:48 -0500

    Added multi-browser support

--- web/app.js
+++ web/app.js
@@ -18,37 +18,61 @@ var main = function () {
 
 			r.onload = function() {
 				file = this.result;
-				console.log(file);
 				return file;
 			}
 			r.readAsText(this.files[0]);
 			});
-		}
+	}
 
 	var dataCreator = function(text) {
 		text = file + ''; // Get rid of annoying console error saying split isn't a function
 		var splitup = text.split('==========');
-		for (var i = 0; i < splitup.length - 1; i++) {
+		for (var i = 1; i < splitup.length - 1; i++) { // First value doesn't conform due to lack of leading equal signs
 			var current = splitup[i].split('\n');
 			
 			var authorLine = current[1].split('('); // First line of each block is Book Title (Author Name)
 			var bookTitle = authorLine[0].trim();
 			var authorName = authorLine.slice(-1)[0].replace(')', '');
 			var quote = current[4].toString();
+			var writeBookSpot = function() {
+					dictionary[authorName][bookTitle] = {};
+					dictionary[authorName][bookTitle]['quote' + i] = quote;
+			};
+
+
 			try {
 				dictionary[authorName][bookTitle]['quote' + i] = quote;
 			}
-			catch (err) {
-				if (err.message === 'dictionary[authorName] is undefined') {
-					dictionary[authorName] = {};
-					dictionary[authorName][bookTitle] = {};
-					dictionary[authorName][bookTitle]['quote' + i] = quote;		
-				}
-				else if (err.message === 'dictionary[authorName][bookTitle] is undefined') {
-					dictionary[authorName][bookTitle] = {};
-					dictionary[authorName][bookTitle]['quote' + i] = quote;					
-				};
+			catch (err) { //Will need to create blank dictioary entries if first time seeing author/book
+				// var writeBookSpot = function() {
+				// 		dictionary[authorName][bookTitle] = {};
+				// 		dictionary[authorName][bookTitle]['quote' + i] = quote;
+				// };
+				// Firefox
+					if (err.message === 'dictionary[authorName] is undefined') {
+						dictionary[authorName] = {};
+						var writeBookSpot = function() {
+						dictionary[authorName][bookTitle] = {};
+						dictionary[authorName][bookTitle]['quote' + i] = quote;	 };
+						writeBookSpot();	
+					}
+					else if (err.message === 'dictionary[authorName][bookTitle] is undefined') {
+						writeBookSpot();					
+					}
+				// Chrome errors
+					else if (err.message === "Cannot read property '"+bookTitle+"' of undefined") {
+						dictionary[authorName] = {};
+						writeBookSpot();
+					}
+					else if (err.message === "Cannot set property 'quote"+i+"' of undefined"){
+						writeBookSpot();
+					};
 			};
+			// finally {
+			// 	dictionary[authorName] = {};
+			// 	dictionary[authorName][bookTitle] = {};
+			// 	dictionary[authorName][bookTitle]['quote' + i] = quote;	
+			// };
 		};
 	return dictionary;
 	};
@@ -57,8 +81,13 @@ var main = function () {
 	getText();
 
 	$('button#process').on('click', function() {
-		dataCreator();
+		dictionary = dataCreator();
 		console.log(dictionary);
+		// var dictString = JSON.stringify(dictionary);
+		// console.log(dictString);
+		// var $content = $('#author-content p');
+		// $content.fadeOut(500).delay(500).empty();
+		// $content.append('<p>'+dictString+'</p>').delay(500).fadeIn(500);
 	});
 
 };
