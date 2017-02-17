442ed6dfb695677256523bf1fe05511d15606ecb
ChemiKyle <Kyle.2493@gmail.com>
2017-02-17 11:25:18 -0500

    Made semifunctioning javascript

---
+++ web/app.js
@@ -0,0 +1,62 @@
+var main = function () {
+	"use strict";
+	
+	var clearLeftPage = function() {
+		$("div.left-page").empty();
+	};
+
+	$("input#file-submit").on("click", function() {
+		clearLeftPage();
+	});
+
+	var fileParser = function(users-file) {
+
+	};
+
+// 	This function works, but skips the first 2 quotes from each author
+	var dataCreator = function(text) {
+		var splitup = text.split('==========');
+		for (var i = 0; i < splitup.length - 1; i++) {
+			var current = splitup[i].split('\n');
+			
+			var authorLine = current[1].split('(');
+			var bookTitle = authorLine[0].trim();
+			var authorName = authorLine.slice(-1)[0].replace(')', '');
+			// console.log(bookTitle, authorName);
+			var quote = current[4].toString();
+			// console.log(quote);
+			try {
+				dictionary[authorName][bookTitle]['quote' + i] = quote;
+			}
+			catch (err) {
+				if (err.message === 'dictionary[authorName] is undefined') {
+					dictionary[authorName] = {};
+				}
+				else if (err.message === 'dictionary[authorName][bookTitle] is undefined') {
+					dictionary[authorName][bookTitle] = {};
+				};
+			};
+		};
+	};
+
+	if (FileReader) {
+
+		var dict = [];
+
+		var fileInput = document.getElementById('file');
+
+		function getText(readFile) {
+			var reader = new FileReader();
+			reader.onload = function(e) {
+				var text = reader.result;
+			}
+			fileDisplayArea.innerText = reader.result;
+			reader.readAsText(readFile);
+		};
+	}
+	else {
+		window.alert("Unfortunately the browser you are using won't allow me to read files!");
+	};
+};
+
+$(document).ready(main);
